9694d5b5d7f35a77f1f78c6cc17639ed0b3f7d0e
CrashMaster8497 <crashmaster8497@gmail.com>
2022-08-20 22:32:45 +0800

    feat: add parrot

---
+++ tests/ZooLibrary.Tests/Animals/Birds/BirdTest.cs
@@ -0,0 +1,20 @@
+﻿using ZooLibrary.Animals.Birds;
+
+namespace ZooLibrary.Tests.Animals.Birds
+{
+    public class BirdTest
+    {
+        [Theory]
+        [MemberData(nameof(GenerateBirds))]
+        public void ShouldBeAbleToCreateBird(Bird bird)
+        {
+            Assert.NotNull(bird);
+        }
+
+        private static IEnumerable<object[]> GenerateBirds()
+        {
+            yield return new object[] { new Parrot() };
+            //yield return new object[] { new Penguin() };
+        }
+    }
+}
