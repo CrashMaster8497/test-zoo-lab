0dcc92b805ac0f070a6b69ad89321b70b7401556
CrashMaster8497 <crashmaster8497@gmail.com>
2022-08-20 22:52:40 +0800

    feat: add snake

---
+++ tests/ZooLibrary.Tests/Animals/Reptiles/ReptileTest.cs
@@ -0,0 +1,20 @@
+﻿using ZooLibrary.Animals.Reptiles;
+
+namespace ZooLibrary.Tests.Animals.Reptiles
+{
+    public class ReptileTest
+    {
+        [Theory]
+        [MemberData(nameof(GenerateReptiles))]
+        public void ShouldBeAbleToCreateReptile(Reptile reptile)
+        {
+            Assert.NotNull(reptile);
+        }
+
+        private static IEnumerable<object[]> GenerateReptiles()
+        {
+            yield return new object[] { new Snake() };
+            //yield return new object[] { new Turtle() };
+        }
+    }
+}
