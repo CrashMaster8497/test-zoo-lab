1f8dbfee6e8889d922bf200d6b01003b71ffeb25
CrashMaster8497 <crashmaster8497@gmail.com>
2022-08-20 18:22:16 +0800

    feat: feedtime

---
+++ src/ZooLibrary/Utility/FeedTime.cs
@@ -0,0 +1,16 @@
+﻿using ZooLibrary.Employees;
+
+namespace ZooLibrary.Utility
+{
+    public class FeedTime
+    {
+        public readonly DateTime DateTime;
+        public readonly ZooKeeper ZooKeeper;
+
+        public FeedTime(DateTime dateTime, ZooKeeper zooKeeper)
+        {
+            DateTime = dateTime;
+            ZooKeeper = zooKeeper;
+        }
+    }
+}
---
+++ tests/ZooLibrary.Tests/Utility/FeedTimeTest.cs
@@ -0,0 +1,14 @@
+﻿namespace ZooLibrary.Tests.Utility
+{
+    public class FeedTimeTest
+    {
+        [Fact]
+        public void ShouldBeAbleToCreateFeedTime()
+        {
+            var feedTime = new ZooLibrary.Utility.FeedTime(dateTime: DateTime.Now, zooKeeper: new Employees.ZooKeeper());
+
+            Assert.NotNull(feedTime);
+            Assert.NotNull(feedTime.ZooKeeper);
+        }
+    }
+}
