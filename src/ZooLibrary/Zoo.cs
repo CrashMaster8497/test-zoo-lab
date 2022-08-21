3feeb1a9f38cbeb52452daa895bf13c478a28cee
CrashMaster8497 <crashmaster8497@gmail.com>
2022-08-21 21:14:10 +0800

    feat: zoo part 1

--- src/ZooLibrary/Zoo.cs
+++ src/ZooLibrary/Zoo.cs
@@ -1,7 +1,65 @@
-﻿namespace ZooLibrary
+﻿using ZooLibrary.Animals;
+using ZooLibrary.Employees;
+using ZooLibrary.Exceptions;
+
+namespace ZooLibrary
 {
     public class Zoo
     {
+        public List<Enclosure> Enclosures { get; set; } = new List<Enclosure>();
+        public List<IEmployee> Employees { get; set; } = new List<IEmployee>();
+        public string Location { get; set; } = string.Empty;
 
+        public Zoo() { }
+        public Zoo(string location)
+        {
+            Location = location;
+        }
+
+        public Enclosure AddEnclosure(string name, int squareFeet)
+        {
+            var enclosure = new Enclosure
+            {
+                Name = name,
+                ParentZoo = this,
+                SquareFeet = squareFeet,
+            };
+
+            Enclosures.Add(enclosure);
+
+            return enclosure;
+        }
+
+        public Enclosure FindAvailableEnclosure(Animal newAnimal)
+        {
+            Enclosure? availableEnclosure = null;
+            foreach (var enclosure in Enclosures)
+            {
+                int sqFtLeft = enclosure.SquareFeet;
+                Animal? notFriendlyAnimal = null;
+                foreach (Animal animal in enclosure.Animals)
+                {
+                    sqFtLeft -= animal.RequiredSpaceSqFt;
+                    if (!newAnimal.IsFriendlyWith(animal) || !animal.IsFriendlyWith(newAnimal))
+                    {
+                        notFriendlyAnimal = animal;
+                    }
+                }
+
+                if (sqFtLeft >= newAnimal.RequiredSpaceSqFt && notFriendlyAnimal == null)
+                {
+                    availableEnclosure = enclosure;
+                }
+            }
+
+            if (availableEnclosure == null)
+            {
+                throw new NoAvailableEnclosureException(string.Format(
+                    "Can't find an available enclosure for animal {0}",
+                    newAnimal.GetType().Name));
+            }
+
+            return availableEnclosure;
+        }
     }
 }
---
+++ tests/ZooLibrary.Tests/ZooTest.cs
@@ -0,0 +1,212 @@
+﻿using ZooLibrary.Animals;
+using ZooLibrary.Animals.Mammals;
+using ZooLibrary.Employees;
+using ZooLibrary.Exceptions;
+
+namespace ZooLibrary.Tests
+{
+    public class ZooTest
+    {
+        [Fact]
+        public void ShouldBeAbleToCreateZoo()
+        {
+            var zoo = new Zoo();
+
+            Assert.NotNull(zoo);
+            Assert.NotNull(zoo.Enclosures);
+            Assert.IsType<List<Enclosure>>(zoo.Enclosures);
+            Assert.Empty(zoo.Enclosures);
+            Assert.NotNull(zoo.Employees);
+            Assert.IsType<List<IEmployee>>(zoo.Employees);
+            Assert.Empty(zoo.Employees);
+            Assert.Equal(string.Empty, zoo.Location);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToCreateZooWithLocation()
+        {
+            var zoo = new Zoo("location");
+
+            Assert.NotNull(zoo);
+            Assert.Equal("location", zoo.Location);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToAddEnclosure()
+        {
+            var zoo = new Zoo();
+
+            zoo.AddEnclosure("name", 1000);
+
+            Assert.True(zoo.Enclosures.Count == 1);
+            Assert.True(zoo.Enclosures[0].Name == "name");
+            Assert.Empty(zoo.Enclosures[0].Animals);
+            Assert.True(zoo.Enclosures[0].ParentZoo == zoo);
+            Assert.True(zoo.Enclosures[0].SquareFeet == 1000);
+        }
+
+        [Theory]
+        [MemberData(nameof(GenerateZooWithAvailableEnclosure))]
+        public void ShouldBeAbleToFindAvailableEnclosure(Zoo zoo, Animal animal)
+        {
+            var enclosure = zoo.FindAvailableEnclosure(animal);
+            Assert.NotNull(enclosure);
+            Assert.Contains(enclosure, zoo.Enclosures);
+
+            enclosure.AddAnimal(animal);
+        }
+
+        [Theory]
+        [MemberData(nameof(GenerateZooWithoutAvailableEnclosure))]
+        public void ShouldThrowNoAvailableEnclosureException(Zoo zoo, Animal animal)
+        {
+            var exception = Assert.Throws<NoAvailableEnclosureException>(() => zoo.FindAvailableEnclosure(animal));
+            Assert.Equal(string.Format("Can't find an available enclosure for animal {0}",
+                animal.GetType().Name), exception.Message);
+        }
+
+        /*
+        [Theory]
+        [MemberData(nameof(GenerateZooAndSuitableEmployee))]
+        public void ShouldBeAbleToHireEmployee(Zoo zoo, IEmployee employee)
+        {
+
+        }
+        */
+
+        private static IEnumerable<object[]> GenerateZooWithAvailableEnclosure()
+        {
+            yield return new object[]
+            {
+                new Zoo
+                {
+                    Enclosures = new List<Enclosure>
+                    {
+                        new Enclosure { Name = "1", SquareFeet = 1000 },
+                        new Enclosure { Name = "2", SquareFeet = 999 },
+                    }
+                },
+                new Bison()
+            };
+            yield return new object[]
+            {
+                new Zoo
+                {
+                    Enclosures = new List<Enclosure>
+                    {
+                        new Enclosure
+                        {
+                            Name = "1",
+                            SquareFeet = 2000,
+                            Animals = new List<Animal> { new Bison() }
+                        },
+                        new Enclosure
+                        {
+                            Name = "2",
+                            SquareFeet = 1999,
+                            Animals = new List<Animal> { new Bison() }
+                        }
+                    }
+                },
+                new Elephant()
+            };
+            yield return new object[]
+            {
+                new Zoo
+                {
+                    Enclosures = new List<Enclosure>
+                    {
+                        new Enclosure
+                        {
+                            Name = "1",
+                            SquareFeet = 2000,
+                            Animals = new List<Animal> { new Bison() }
+                        },
+                        new Enclosure
+                        {
+                            Name = "2",
+                            SquareFeet = 2000,
+                            Animals = new List<Animal> { new Lion() }
+                        }
+                    }
+                },
+                new Elephant()
+            };
+        }
+
+        private static IEnumerable<object[]> GenerateZooWithoutAvailableEnclosure()
+        {
+            yield return new object[]
+            {
+                new Zoo { Enclosures = new List<Enclosure>() },
+                new Bison()
+            };
+            yield return new object[]
+            {
+                new Zoo
+                {
+                    Enclosures = new List<Enclosure>
+                    {
+                        new Enclosure { Name = "1", SquareFeet = 999 }
+                    }
+                },
+                new Bison()
+            };
+            yield return new object[]
+            {
+                new Zoo
+                {
+                    Enclosures = new List<Enclosure>
+                    {
+                        new Enclosure
+                        {
+                            Name = "1",
+                            SquareFeet = 1999,
+                            Animals = new List<Animal> { new Bison() }
+                        }
+                    }
+                },
+                new Bison()
+            };
+            yield return new object[]
+            {
+                new Zoo
+                {
+                    Enclosures = new List<Enclosure>
+                    {
+                        new Enclosure
+                        {
+                            Name = "1",
+                            SquareFeet = 2000,
+                            Animals = new List<Animal> { new Lion() }
+                        }
+                    }
+                },
+                new Bison()
+            };
+        }
+
+        /*
+        private static IEnumerable<object[]> GenerateZooAndSuitableEmployee()
+        {
+            yield return new object[]
+            {
+                new Zoo()
+                {
+                    Enclosures = new List<Enclosure>
+                    {
+                        new Enclosure
+                        {
+                            Animals = { new Bison() }
+                        }
+                    }
+                },
+                new ZooKeeper
+                {
+                    AnimalExperiences = { "Bison" }
+                }
+            };
+        }
+        */
+    }
+}
