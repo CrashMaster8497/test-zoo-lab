b026f404c0538f2d13663229f11a3c1ed29742cc
CrashMaster8497 <crashmaster8497@gmail.com>
2022-08-22 02:11:46 +0800

    feat: zoo app

---
+++ src/ZooApp/ZooApp.cs
@@ -0,0 +1,25 @@
+﻿using ZooLibrary;
+
+namespace ZooApp
+{
+    public class ZooApp
+    {
+        private List<Zoo> _zoos = new List<Zoo>();
+
+        public void AddZoo(Zoo zoo)
+        {
+            _zoos.Add(zoo);
+        }
+
+        public void Run()
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            var zooApp = new ZooApp();
+            zooApp.Run();
+        }
+    }
+}
